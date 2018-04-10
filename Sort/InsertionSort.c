53cbc2e1ad1c6f1a089a36f1b6e891d0615ffa96
parkjk93 <38125895+parkjk93@users.noreply.github.com>
2018-04-10 14:43:47 +0900

    Rename InsertionSort.c to Sort/InsertionSort.c

---
+++ Sort/InsertionSort.c
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+#define SIZE 100
+
+void Swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void InsertionSort(int *data)
+{
+	for (int i=1 ; i<SIZE ; i++)
+	{
+		for (int j=i ; j>0 ; j--)
+		{
+			if (data[j] < data[j - 1])
+				Swap(&data[j], &data[j - 1]);
+		}
+	}
+}
+
+int main()
+{
+	int data[SIZE];
+	for (int i = 0; i<SIZE; i++)
+		data[i] = SIZE - i;
+
+	InsertionSort(data);
+
+	for (int i = 0; i < SIZE; i++)
+		printf("%d ", data[i]);
+
+    return 0;
+}
+
